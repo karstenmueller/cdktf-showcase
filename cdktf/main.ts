ad0e7c1fad372b7fc2e66714d140d80778f52333
Karsten Müller <karsten.mueller@fielmann.com>
2021-08-11 16:05:27 +0200

    integrate app code

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -17,7 +17,7 @@ class LambdaStack extends TerraformStack {
     // Create Lambda executable including Cloudwatch Logs and IAM execution role
     // Code will be bundled via esbuild and inline all dependencies
     const lambda = new NodejsFunction(this, "lambda-function", {
-      path: path.join(__dirname, '../application', 'index.ts'),
+      path: path.join(__dirname, './app', 'index.ts'),
       timeout: 90,
       memorySize: 128,
     });
